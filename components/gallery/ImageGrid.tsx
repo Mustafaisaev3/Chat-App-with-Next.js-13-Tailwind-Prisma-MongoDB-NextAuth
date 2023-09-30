719af53299d7c07f6ebe741f5292a0bb6f48a67f
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-09-30 23:03:37 +0300

    Making conditional rendering of images grid

--- components/gallery/ImageGrid.tsx
+++ components/gallery/ImageGrid.tsx
@@ -15,9 +15,9 @@ const ImageGrid: React.FC<ImageGridProps> = ({ currentUser }) => {
     <>
       <GalleryTopbar currentUser={currentUser} />
       <div className='w-full h-auto flex flex-col sm:grid grid-cols-gallery auto-rows-[10px] gap-x-[10px] p-4 bg-[#2a2a2a]' >
-        {currentUser.images.map((image) => {
+        {currentUser.images ? (currentUser.images.map((image) => {
           return <ImageItem image={image} key={image.id} currentUser={currentUser}  />
-        })}
+        })) : null}
       </div>
     </>
   )
