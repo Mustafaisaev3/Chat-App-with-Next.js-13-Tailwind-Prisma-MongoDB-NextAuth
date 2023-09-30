c4e55444b37081a641d732583d92d3a6faac0cb0
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-09-30 23:35:22 +0300

    Change images render type

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,9 +1,8 @@
 import ImageGrid from "@/components/gallery/ImageGrid"
 import getCurrentUser from "../actions/getCurrentUser"
-import getGalleryImages from "../actions/getGalleryImages"
 
 const Gallery = async () => {
-  const currentUser = await getCurrentUser()
+  const currentUser = await getCurrentUser() 
 
   return (
     <div className="lg:block h-full min-h-screen bg-[#2a2a2a]">
